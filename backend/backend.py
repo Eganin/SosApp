a9bcadfd0d94c850f20ddf9107411f5399d641ae
Eganin <egorzaharin12@gmail.com>
2020-11-03 23:29:10 +0500

    sql query write

--- backend/backend.py
+++ backend/backend.py
@@ -11,44 +11,64 @@ app.config.from_object(__name__)
 app.config.update(DATABASE=os.path.join(app.root_path, 'ssdetec.db'))
 
 
-def connect_db() :
+def connect_db():
     rv = sq.connect(app.config['DATABASE'])
     rv.row_factory = sq.row
     return rv
 
+
 def get_db():
-    if hasattr(g, 'sqlite_db'):
+    if hasattr(g, 'ssdetec.db'):
         g.sqlite_db = connect_db
     return g.sqlite_db
+
 
 # POST
 @app.route('/login', methods=["POST"])
 def hello_user():
-    json = request.get_json()
-    db =get_db()
-    dbase = FDataBase(db)
-    if json('name') != dbase.execute("SELECT INTO users(name, password) WHERE name == {json = ['name']}"):
-        return False
-    elif json('password') != dbase.execute("SELECT INTO users(password) WHERE password == {json = ['password']}"):
-        return False
-    return True
+    try:
+        json = request.get_json()
+        print(json)
+        db = get_db()
+        dbase = FDataBase(db)
+        if json['name'] != dbase.execute("SELECT INTO users (name) WHERE name = ?", json["name"]):
+            dbase.commit()
+            dbase.close()
+            return "false"
+        elif json['password'] != dbase.execute("SELECT INTO users (password) WHERE password = ?", json["password"]):
+            dbase.commit()
+            dbase.close()
+            return "false"
+        else:
+            return "true"
+
+    except Exception as e:
+        return "false"
 
 
 # POST
 @app.route('/registration', methods=['POST'])
 def get_text_prediction():
-    json = request.get_json()
-    db = get_db()
-    dbase = FDataBase(db)
-    dbase.execute("INSERT INTO users(name) VALUES {json = ['name']} ")
-    dbase.execute("INSERT INTO users(email) VALUES {json = ['email']} ")
-    dbase.execute("INSERT INTO users(password) VALUES {json = ['password']} ")
-    dbase.execute("INSERT INTO users(phone_user) VALUES {json = ['number']} ")
-    return True
+        json = request.get_json()
+        print(json)
+        db = get_db()
+        dbase = FDataBase(db)
+
+        dbase.execute("INSERT INTO users (name) VALUES (?) ", json["name"])
+        dbase.execute("INSERT INTO users (email) VALUES (?) ", json["email"])
+        dbase.execute("INSERT INTO users (password) VALUES (?) ", json["password"])
+        dbase.execute("INSERT INTO users (phone_user) VALUES (?) ", json["number"])
+        dbase.commit()
+        dbase.close()
+        return "true"
+
+
+
 
 def close_db(error):
-    if hasattr(g, 'sqlite_db'):
+    if hasattr(g, 'ssdetec.db'):
         g.sqlite_db.close()
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="25.72.37.220")
