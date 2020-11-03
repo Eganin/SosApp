59e7c4ac0549917300ba0ceaab4d27760c5fdb93
Eganin <egorzaharin12@gmail.com>
2020-11-04 00:45:13 +0500

    refactoring backend

--- backend/backend.py
+++ backend/backend.py
@@ -2,24 +2,28 @@ from flask import Flask, request, session, g, redirect, url_for, jsonify
 import sqlite3 as sq
 import os
 
-DATABASE = '/template/ssdetec.db'
+DATABASE = 'C:\\Users\\Eganin\\AndroidStudioProjects\\TestProjectSchool\\SosApplication\\backend\\template\\ssdetec.db'
 SECRET_KEY = 'hzzachem'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config.update(DATABASE=os.path.join(app.root_path, 'ssdetec.db'))
+app.config.update(DATABASE=DATABASE)
 
 
 def connect_db():
-    rv = sq.connect(app.config['DATABASE'])
-    rv.row_factory = sq.row
+    rv = sq.connect(DATABASE)
     return rv
 
 
+@app.before_request
+def before_request():
+    g.sqlite_db = connect_db()
+
+
 def get_db():
-    if hasattr(g, 'ssdetec.db'):
-        g.sqlite_db = connect_db
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
     return g.sqlite_db
 
 
@@ -27,21 +31,20 @@ def get_db():
 @app.route('/login', methods=["POST"])
 def hello_user():
     try:
+        conn = sq.connect(DATABASE)
+        cur = conn.cursor()
         json = request.get_json()
         print(json)
-        db = get_db()
-        dbase = FDataBase(db)
-        if json['name'] != dbase.execute("SELECT INTO users (name) WHERE name = ?", json["name"]):
-            dbase.commit()
-            dbase.close()
+        if json['name'] != cur.execute("SELECT INTO users (name) WHERE name = ?", json["name"]):
+            conn.commit()
+            conn.close()
             return "false"
-        elif json['password'] != dbase.execute("SELECT INTO users (password) WHERE password = ?", json["password"]):
-            dbase.commit()
-            dbase.close()
+        elif json['password'] != cur.execute("SELECT INTO users (password) WHERE password = ?", json["password"]):
+            cur.commit()
+            cur.close()
             return "false"
         else:
             return "true"
-
     except Exception as e:
         return "false"
 
@@ -49,26 +52,20 @@ def hello_user():
 # POST
 @app.route('/registration', methods=['POST'])
 def get_text_prediction():
-        json = request.get_json()
-        print(json)
-        db = get_db()
-        dbase = FDataBase(db)
-
-        dbase.execute("INSERT INTO users (name) VALUES (?) ", json["name"])
-        dbase.execute("INSERT INTO users (email) VALUES (?) ", json["email"])
-        dbase.execute("INSERT INTO users (password) VALUES (?) ", json["password"])
-        dbase.execute("INSERT INTO users (phone_user) VALUES (?) ", json["number"])
-        dbase.commit()
-        dbase.close()
-        return "true"
-
-
+    conn = sq.connect(DATABASE)
+    cur = conn.cursor()
+    json = request.get_json()
+    print(json)
+    cur.execute("INSERT INTO users (name,password,phone_user,email) VALUES (?,?,?,?) ",
+                (json["name"], json["password"], json["number"], json["email"]))
+    conn.commit()
+    conn.close()
+    return "true"
 
 
 def close_db(error):
     if hasattr(g, 'ssdetec.db'):
         g.sqlite_db.close()
-
 
 if __name__ == '__main__':
     app.run(host="25.72.37.220")
